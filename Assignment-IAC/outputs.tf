55b2929fe37733977c4ab74f75b8f1ccbd98ea43
Naren-15 <narendran1506@gmail.com>
2023-03-26 19:31:41 +0530

    initial commit

---
+++ Assignment-IAC/outputs.tf
@@ -0,0 +1,16 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+[ec2-user@ip-172-31-36-61 terraform]$ cat outputs.tf 
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
---
+++ Assignment-IAC/terraform.tfvars
@@ -0,0 +1,10 @@
+aws_access_key = "AKIASD2SPQXE22TWSR7N"
+aws_secret_key = "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"
+
+region                   = "ap-south-1"
+availability_zones_count = 2
+
+project = "TFEKSWorkshop"
+
+vpc_cidr         = "10.0.0.0/16"
+subnet_cidr_bits = 8
